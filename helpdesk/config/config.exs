312b1a1740c1b0668df3644cf993386e8a3dbfea
Jack Corley <jdcorley@gmail.com>
2023-02-04 19:47:26 +0000

    created first resource

--- helpdesk/config/config.exs
+++ helpdesk/config/config.exs
@@ -1,4 +1,4 @@
 import Config
 
 # if you already have a config file, then you'll just need to add this:
-config :ash, :use_all_identities_in_manage_relationship?, false
+config :ash, :helpdesk, :ash_apis :use_all_identities_in_manage_relationship?, false
---
+++ helpdesk/lib/helpdesk/support/registry.ex
@@ -0,0 +1,13 @@
+# lib/helpdesk/support/registry.ex
+
+defmodule Helpdesk.Support.Registry do
+  use Ash.Registry,
+    extensions: [
+      # This extension adds helpful compile time validations
+      Ash.Registry.ResourceValidations
+    ]
+
+  entries do
+    entry Helpdesk.Support.Ticket
+  end
+end
